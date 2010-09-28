3a0a1d9a3ed888313c718b291dfbd7cbb60bad5b
jko <jhkoivis@gmail.com>
2010-09-28 13:22:17 +0300

    test commit

---
+++ run_globtests.py
@@ -0,0 +1,10 @@
+import glob, trace, unittest, sys
+
+""" argv[1:] is globbing. runs tests matching to the glob (aka sh wildcards) """
+
+if __name__=="__main__":
+	for g in sys.argv[1:]:
+		modules = map(trace.fullmodname, glob.glob(g))
+		suite = unittest.TestLoader().loadTestsFromNames(modules)
+		print "Running a test (globbing): " + g
+		unittest.TextTestRunner().run(suite)
---
+++ test_configparser.py
@@ -0,0 +1,48 @@
+
+from configparser import ConfigParser
+from masterdata import MasterData
+from unittest import TestCase
+
+import os
+import inspect
+
+class test_configparser(TestCase):
+	
+	def setUp(self):
+		folder	= os.path.join("testsuite","test_masterdata")
+		self.mockmaster = MasterData()
+		self.configparser = ConfigParser(os.path.join(folder,"test_masterdata1.dicconf"), self.mockmaster, 'test_masterdata.py')
+		self.configparser.parse()
+
+	def testIntegers(self):
+		self.assertEquals(self.mockmaster.get("int"), 5)
+		self.assertEquals(self.mockmaster.get("intWithWhitespace"), 32)
+		self.assertEquals(self.mockmaster.get("intWithComment"), 7)
+
+	def testFloats(self):
+		self.assertAlmostEqual(self.mockmaster.get("float"), 3.14)
+		self.assertAlmostEqual(self.mockmaster.get("floatWithWhitespace"), 42.11322)
+		self.assertAlmostEqual(self.mockmaster.get("floatWithComment"), 11.27)
+
+	def testTuples(self):
+		self.assertEquals(self.mockmaster.get("tuple"), (3.14,1))
+		self.assertEquals(self.mockmaster.get("tupleWithWhitespace"), (42.11322,32))
+		self.assertEquals(self.mockmaster.get("tupleWithComment"), (11
+																    ,27))
+
+	def testStrings(self):
+		self.assertEquals(self.mockmaster.get("string"), "3.14 = about pii")
+		self.assertEquals(self.mockmaster.get("stringWithWhitespace"), "	whitespace   ")
+		self.assertEquals(self.mockmaster.get("stringWithComment"), "stringtocomment")
+
+	def testRegularExpressions(self):
+		self.assertEquals(self.mockmaster.get("reg"), "(?P<3\.14>\w+)\.(?P<pii>\w+)\.txt")
+		self.assertEquals(self.mockmaster.get("regWithWhitespace"),"(?P<		whitespace>\w+)")
+		self.assertEquals(self.mockmaster.get("regWithComment"), "(?P<stringtocomment>\w+)")
+
+	def testGetandSet(self):
+		key = "number"
+		value = 75.25
+		self.mockmaster.set(key, value)
+		self.assertAlmostEqual(self.mockmaster.get(key), 75.25)
+
