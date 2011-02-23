fea822a11a595ad0876c21368544e2473eaa3ad0
Simo Tuomisto <st.tuomisto@gmail.com>
2011-02-23 08:57:56 +0200

    Verbose folderanalyzer2.py

---
+++ imagelist.py
@@ -0,0 +1,27 @@
+
+class ImageList:
+    
+    def __init__(self, folderobject, imageClass, seqFilter,  imgFilters, regExpression=None):
+        unorderedList = self._getFilteredFolder(folderobject, imgFilters, imageClass, regExpression)
+        self.imagelist = seqFilter.filter(unorderedList)
+        
+    def _getFilteredFolder(self, folderobject, imgFilters, imageClass, regExpression=None):
+                
+        imagelist = []
+        for filename in folderobject:
+            imageObject = imageClass(filename, regExpression)
+            appendBoolean = True
+            for imageFilter in imgFilters:
+                if not imageFilter.filter(imageObject):
+                    appendBoolean = False
+                    break
+            if appendBoolean:
+                imagelist.append(imageObject)
+        return imagelist
+            
+    def next(self):
+        return self.imageiterator.next()
+    
+    def __iter__(self):
+        self.imageiterator = iter(self.imagelist)
+        return self
---
+++ test_dffexporter.py
@@ -0,0 +1,54 @@
+
+from unittest import TestCase
+from dffexporter import DffExportParameters, DffExporter
+from diccore import Dic
+
+import os
+
+class MockDeformationData:
+	# XXX: mock tells us that we are VERY coupled to the deformationdata internal structure
+	def __init__(self):
+		self.dic = Dic()
+		self.data = {"Crop" : (100,100,120,120), 
+					 "PictureSize" : (100,100), 
+					 "PictureData1":{"key":"in","key":"is"}, 
+					 "PictureData2":{"complicated":"stuff"}, 
+					 "FirstPictureName":"name1",
+					 "SecondPictureName":"name2"}
+	def get(self, key):
+		return self.data[key]
+
+	def getDeformationAtPoints(self, pointarray):
+		self.getDeformationAtPointsCalled = True
+		return (3.4, 4.5)
+
+class test_DffExporter(TestCase):
+	
+	def setUp(self):
+		self.mockdeformation = MockDeformationData()
+		self.exportparameters = DffExportParameters(overwrite=True)
+		# XXX: dependency from fs resource
+		self.testfilename = 'test.dff'
+
+	def testInit(self):
+		exporter = DffExporter(self.mockdeformation, 
+									self.exportparameters, 
+									self.testfilename)
+		self.assertTrue(exporter.deformation)
+		self.assertTrue(exporter.exportparameters)
+		self.assertTrue(exporter.outputfilename)
+		
+		
+	def testExportData(self):
+		exporter = DffExporter(self.mockdeformation, 
+							   self.exportparameters, 
+							   self.testfilename)
+		self.assertTrue(exporter.export())
+		self.assertTrue(self.mockdeformation.getDeformationAtPointsCalled)
+		self.assertTrue(os.path.exists(self.testfilename))
+		# XXX: should assert data
+
+	def tearDown(self):
+		if os.path.exists(self.testfilename):
+			os.remove(self.testfilename)
+	
