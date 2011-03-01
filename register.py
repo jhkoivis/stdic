a5d2181fddd3649d0f9a36fbfa52519f693601ba
Simo Tuomisto <st.tuomisto@gmail.com>
2011-03-01 14:47:31 +0200

    Final cleaning.

--- register.py
+++ register.py
@@ -1,14 +1,25 @@
+#!/usr/bin/python
+"""
+
+    Simo Tuomisto, 2010
+
+"""
+
+from folderanalyzer import FolderAnalyzer
+from configparser import ConfigParser
+from masterdata import MasterData
+
+import sys
+
 
 from lib.analyzer import Analyzer
 from lib.configparser import ConfigParser
 from lib.folderscan import FolderScan
 import sys
 
-if __name__=="__main__":
+class Register:
     
-        folder = sys.argv[1]
-        dfffolder = sys.argv[2]
-        configurationfile = sys.argv[3]
+    def __init__(self, folder, dfffolder, configurationfile):
         
         #--------------------------------------------------------
         # Configuration parsing
@@ -23,4 +34,17 @@ if __name__=="__main__":
         filelist = folder.findWithExpression(imageformat)
                 
         analyzer = Analyzer(filelist, dfffolder, config)
-        analyzer.analyze()
+
+    def run(self):
+        self.analyzer.analyze()
+
+if __name__ == "__main__":
+    
+    if len(sys.argv) != 3:
+        print "Usage: imagefolder dfffolder configfile"
+    else:
+        folder = sys.argv[1]
+        dfffolder = sys.argv[2]
+        configurationfile = sys.argv[3]
+        register = Register(folder, dfffolder, configurationfile)
+        register.run()
