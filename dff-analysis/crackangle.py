9b15a74a57a060ce6ed791ff55363022f95f834c
jko <jhkoivis@gmail.com>
2011-03-02 16:17:33 +0200

    fix case collision

--- dff-analysis/crackangle.py
+++ dff-analysis/crackangle.py
@@ -4,51 +4,154 @@ from math import *
 from dffreader import DffReader
 from scipy.ndimage.interpolation import map_coordinates
 
-class CrackAngle:
+class CrackPoint:
 
-	def __init__(self, dffFilename):
+	def __init__(self, filename):
 	
-		dff = DffReader(dffFilename)
-			
-		diffY = dff.defY - dff.origY
-		self.derY = np.diff(diffY, 1, 0)
-		self.step = dff.step
+		dff = DffReader(filename)
+	
+		self.origs = dff.getOrigs()
+		self.defs = dff.getDefs()
 		
-	def setCoords(self,xcoord,ycoord,angle):
+		self.diffs = (self.defs[0] - self.origs[0], self.defs[0] - self.origs[0])
+		self.step = dff.getStep()
 		
-		xcoord = float(xcoord)/self.step
-		ycoord = float(ycoord)/self.step
+	def getE(self, axis, xcoord, ycoord, angle):
+	
+		derarray = np.diff(self.diffs[axis], 1, axis)
+		
+		xcoord = xcoord/self.step
+		ycoord = ycoord/self.step
 	
 		angle = radians(angle)
 	
-		self.xcoords = np.array([], dtype=float)
-		self.ycoords = np.array([], dtype=float)
+		xcoords = np.array([], dtype=float)
+		ycoords = np.array([], dtype=float)
 		
 		xcoordplus	= cos(angle)/self.step
 		ycoordplus	= - sin(angle)/self.step
-		maxX		= self.derY.shape[1] - 1
-		maxY		= self.derY.shape[0] - 1
+		maxX		= derarray.shape[1] - 1
+		maxY		= derarray.shape[0] - 1
 		
 		while True:
 			if xcoord < 0 or xcoord >= maxX or ycoord <= 0 or ycoord >= maxY:
 				break
-			self.xcoords = np.append(self.xcoords, xcoord)
-			self.ycoords = np.append(self.ycoords, ycoord)
+			xcoords = np.append(xcoords, xcoord)
+			ycoords = np.append(ycoords, ycoord)
 			
 			xcoord += xcoordplus
 			ycoord += ycoordplus
 		
-	def getE(self, xcoord, ycoord, angle):
-		
-		self.setCoords(xcoord, ycoord, angle)
-		
-		return map_coordinates(self.derY, [self.ycoords, self.xcoords], order=3)
+		return map_coordinates(derarray, [ycoords, xcoords], order=3)
+						
+	def plot(self, array):
 	
-	def getEs(self, xcoord, ycoord):
+		import matplotlib.pyplot as mpl
 		
-		self.setCoords(xcoord, ycoord, 45)
-		e1 = map_coordinates(self.derY, [self.ycoords, self.xcoords], order=3)
-		self.setCoords(xcoord, ycoord, -45)
-		e2 = map_coordinates(self.derY, [self.ycoords, self.xcoords], order=3)
+		mpl.figure
+		mpl.contourf(array,50)
+		mpl.axis("image")
+		mpl.colorbar()
+		mpl.gca().invert_yaxis()
+		mpl.show()
+				
+	def plotE(self, rarray, ratio=0.25):
 	
-		return [e1,e2]
+		import matplotlib.pyplot as mpl
+		
+		interval = (10,len(rarray)*ratio)
+		#interval = (200,350)
+		
+		raxis	= np.arange(0, rarray.shape[0])[interval[0]:interval[1]]
+		rarray	= rarray[interval[0]:interval[1]]
+		
+		mpl.figure
+		
+		mpl.subplot(2,1,1)
+		mpl.plot(raxis, rarray)
+		mpl.axis("tight")
+	
+		rarray = np.log(rarray)
+		
+		raxis = np.log(raxis)
+		
+		mpl.subplot(2,1,2)
+		mpl.plot(raxis, rarray)
+		
+		coefs = np.polyfit(raxis,rarray,1)
+		
+		mpl.hold(True)
+		mpl.plot(raxis, coefs[0]*raxis + coefs[1], label='slope %f' % coefs[0])
+		
+		mpl.axis("tight")
+		mpl.legend()
+		mpl.show()
+				
+	def plotAnglesLog(self, axis, xcoord, ycoord):
+	
+		import matplotlib.pyplot as mpl
+		
+		for angle in xrange(-45,46,15):
+
+			rarray	= self.getE(axis, xcoord, ycoord, angle)
+			raxis	= np.arange(0, rarray.shape[0])
+			
+			rarray = rarray[0:50]
+			raxis = raxis[0:50]
+
+			mpl.figure(1)
+			mpl.plot(raxis, rarray, label='angle %d' % angle)
+			mpl.legend()
+			
+			mpl.axis("tight")
+			mpl.hold(True)
+			coefs = np.polyfit(np.log(raxis),np.log(rarray),1)
+
+			mpl.figure(2)
+			mpl.plot(np.log(raxis), np.log(rarray),'o')
+			mpl.hold(True)
+			mpl.plot(np.log(raxis), coefs[0]*np.log(raxis) + coefs[1], label='angle: %d slope: %f' % (angle, coefs[0]))
+			mpl.legend()
+
+			mpl.figure(3)
+			mpl.plot(angle, coefs[0], 's', label='value %f' % coefs[0])
+			mpl.axis("tight")
+			mpl.figure(4)
+			mpl.plot(angle, coefs[1], 'o', label='value %f' % coefs[1])
+			#mpl.legend()
+			
+			mpl.axis("tight")
+			mpl.hold(True)
+			
+		mpl.show()
+		
+	def plotAngles(self, axis, xcoord, ycoord):
+	
+		import matplotlib.pyplot as mpl
+		
+		mpl.figure
+		
+		for angle in xrange(-90, 90, 15):
+			rarray	= self.getE(axis, xcoord, ycoord, angle)
+			raxis	= np.arange(0, rarray.shape[0])
+			mpl.plot(raxis, rarray, label='angle %d' % angle)
+			mpl.legend()
+			mpl.hold(True)
+		mpl.axis("tight")
+		
+		mpl.show()
+		
+if __name__=="__main__":
+
+	filename	= sys.argv[1]
+	axis		= int(sys.argv[2])
+	xcoord		= float(sys.argv[3])
+	ycoord		= float(sys.argv[4])
+	#angle		= float(sys.argv[5])
+	ratio		= 1
+	
+	crack = CrackPoint(filename)
+	crack.plotAnglesLog(axis, xcoord, ycoord)
+	#E = crack.getE(axis, xcoord, ycoord, angle)
+	#crack.plotE(E, ratio)
+	
