f14973c24ed89e050a4d7af4c2a9240d9bde25dd
jko <jhkoivis@gmail.com>
2011-03-30 23:29:41 +0300

    added imageLoaders

--- dff-analysis/jko/viewer.py
+++ dff-analysis/jko/viewer.py
@@ -20,6 +20,8 @@ window_panel_height = 200
 window_image_width = window_panel_width
 window_image_height = window_panel_height
 
+window_textbox_value = "bitmapProducer.py"
+
 class Positioner:
     
     def createNewPlace(self, parent, compDict):
@@ -57,8 +59,9 @@ class JFrame(wx.Frame):
                           size = (frame_width, frame_height))
         
         self.splitter = wx.SplitterWindow(self)
-        self.tWindow = TopWindow(self.splitter)
+        
         self.bWindow = BottomWindow(self.splitter)
+        self.tWindow = TopWindow(self.splitter, self.bWindow)
         self.splitter.SplitHorizontally(self.tWindow, self.bWindow, window_top_height)
         
 class BottomWindow(wx.Window):
@@ -66,17 +69,17 @@ class BottomWindow(wx.Window):
     def __init__(self, parent):
         wx.Window.__init__(self,parent, -1)
         self.compDict = {}
-        DictTextCtrl(self, -1, "filename NW", col = 0)
-        DictTextCtrl(self, -1, "filename NE", col = 0)
-        DictTextCtrl(self, -1, "filename SW", col = 0)
-        DictTextCtrl(self, -1, "filename SE", col = 0)
+        DictTextCtrl(self, -1, "filename NW", col = 0, value = "bitmapProducerNW.py")
+        DictTextCtrl(self, -1, "filename NE", col = 0, value = "bitmapProducerNE.py")
+        DictTextCtrl(self, -1, "filename SW", col = 0, value = "bitmapProducerSW.py")
+        DictTextCtrl(self, -1, "filename SE", col = 0, value = "bitmapProducerSE.py")
         DictTextCtrl(self, -1, "imageFolder", col = 1)
         DictTextCtrl(self, -1, "dffFolder", col = 1)
         DictTextCtrl(self, -1, "pictureNumber", col = 1)
 
 class DictTextCtrl(wx.TextCtrl, Positioner):
     
-    def __init__(self, parent, id, name, col = 0):
+    def __init__(self, parent, id, name, col = 0, value = window_textbox_value):
         # store defaults
         self.name = name
         self.parent = parent
@@ -95,13 +98,14 @@ class DictTextCtrl(wx.TextCtrl, Positioner):
                              parent,
                              -1,
                              pos = tcPos,
-                             size = size)
+                             size = size,
+                             value = value)
         # add yourself to the list
         parent.compDict[name] = self
         
 class ImagePanel(wx.Panel, Positioner):
     
-    def __init__(self, parent, name, col = 0):
+    def __init__(self, parent, name, col = 0, bitmapProducerFn=None):
         
         self.parent = parent
         self.name = name
@@ -113,43 +117,27 @@ class ImagePanel(wx.Panel, Positioner):
                  size = (window_panel_width, window_panel_height))
         self.parent.compDict[name] = self
         
-        image = wx.Bitmap("lena.png")
+        self.loadBitmap(bitmapProducerFn)
+        
+    def loadBitmap(self, bitmapProducerFn):
+        
+        if bitmapProducerFn == None: return
+        print bitmapProducerFn
+        execfile(bitmapProducerFn)
+        image = bitmap
         image.SetSize((window_image_width, window_image_height))
         wx.StaticBitmap(self, -1, image, size = (window_image_width, window_image_height))
         
 class TopWindow(wx.Window):
     
-    def __init__(self, parent): 
-        wx.Window.__init__(self,parent, -1, size = (window_top_width, window_top_height)) 
-        
+    def __init__(self, parent, bWindow): 
+        wx.Window.__init__(self, parent, -1, size = (window_top_width, window_top_height))
         self.compDict = {}
+        ImagePanel(self, "im NW", col = 0, bitmapProducerFn = bWindow.compDict["filename NW"].GetValue())
+        ImagePanel(self, "im NE", col = 1, bitmapProducerFn = bWindow.compDict["filename NE"].GetValue())
+        ImagePanel(self, "im SW", col = 0, bitmapProducerFn = bWindow.compDict["filename SW"].GetValue())
+        ImagePanel(self, "im SE", col = 1, bitmapProducerFn = bWindow.compDict["filename SE"].GetValue())
         
-        ImagePanel(self, "im1", col = 0)
-        ImagePanel(self, "im2", col = 1)
-        ImagePanel(self, "im1", col = 0)
-        ImagePanel(self, "im1", col = 1)
-        
-        #self.panel=wx.Panel(self,-1, size = (window_panel_width, window_panel_height))
-
-        """ Start Pie Chart Code"""
-        # make a square figure and axes
-        #pylab.figure(1, figsize=(6,6))
-        #ax = pylab.axes([0.1, 0.1, 0.8, 0.8])
-
-        #labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
-        #fracs = [15,30,45, 10]
-
-        #explode=(0, 0.05, 0, 0)
-        #pylab.pie(fracs, explode=explode, labels=labels, autopct='%1.1f%%', shadow=True)
-        #pylab.title('Raining Hogs and Dogs', bbox={'facecolor':'0.8', 'pad':5})
-
-        #plt.show()
-        #pylab.savefig("test.png")
-
-
-        """ End Pie Chart Code"""
-
-
 #if __name__ == "__main__":
     
 app = wx.App()
